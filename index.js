3ec19936f8e6e81375c40810efb6cee9363a3630
Majharulislam <majhuruislam535@gmail.com>
2025-01-07 00:02:20 +0600

    add volunteer post method

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(express.json());
 app.use(cors());
 
 
+
+
+app.get('/',async(req,res)=>{
+   res.send("hello world");
+})
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5g7cb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -25,17 +32,28 @@ async function run() {
      
     await client.connect();
 
+     
+    const database1 = client.db("Volunteer_need_post");
+    const Volunteer_need_post = database1.collection("Volunteer");
 
+    app.post('/volunteer_need_post', async (req, res) => {
+      const data = req.body;
+      const result = await Volunteer_need_post.insertOne(data);
+      res.send(result);
+    })
 
-
-
+    app.get('/volunteer_needs', async (req, res) => {
+      const cursor = Volunteer_need_post.find().sort({ Deadline: 1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
      
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
